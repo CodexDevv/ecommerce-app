7a2fa8f32cb1c13169c9bc05b257dde4995e1c19
CodexDev <stefanga2020@gmail.com>
2022-05-03 21:26:35 +0300

    Added `ForgotPassword.tsx` (for now has the same problem as `SignupDialog.tsx`)

--- components/dialogs/ForgotPassword.tsx
+++ components/dialogs/ForgotPassword.tsx
@@ -8,16 +8,9 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import InputAdornment from '@mui/material/InputAdornment';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
-import SignupDialog from './SignupDialog'
-import ForgotPassword from './ForgotPassword'
-
 type Props = {
   open: boolean,
   handleClose: any,
-}
-
-function timeout(delay: number) {
-  return new Promise(res => setTimeout(res, delay));
 }
 
 const Transition = React.forwardRef(function Transition(
@@ -29,27 +22,7 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="left" ref={ref} {...props} />;
 });
 
-const LoginDialog = ({ open, handleClose }: Props) => {
-
-  const [signupOpen, setSignupOpen] = useState<boolean>(false);
-  const [forgotOpen, setForgotOpen] = useState<boolean>(false);
-
-  const handleSignupOpen = () => {
-    setSignupOpen(true);
-  }
-
-  const handleSignupClose = () => {
-    setSignupOpen(false);
-  }
-
-  const handleForgotOpen = () => {
-    setForgotOpen(true);
-  }
-
-  const handleForgotClose = () => {
-    setForgotOpen(false);
-  }
-
+const ForgotPassword = ({ open, handleClose }: Props) => {
 
   return (
     <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition} PaperProps={{
@@ -71,10 +44,10 @@ const LoginDialog = ({ open, handleClose }: Props) => {
 
       <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', marginRight: 'auto', marginLeft: 'auto', marginTop: 3, padding: { xs: 3, md: 5 }, maxWidth: '440px' }}>
         <Box>
-          <Typography variant="h5" fontWeight={700}>Sign In</Typography>
+          <Typography variant="h5" fontWeight={700}>Reset your password</Typography>
           <Box sx={{ display: 'flex', alignItems: 'end', marginBottom: 2 }}>
-            <Typography mt={1} variant="subtitle2">Don't have an account?</Typography>
-            <Link component="button" underline="none" sx={{ paddingLeft: 0.5, marginBottom: { xs: 0.45, md: 0.35 }, fontWeight: 600 }} onClick={handleSignupOpen}>Register</Link>
+            <Typography mt={1} variant="subtitle2">Remember your password?</Typography>
+            <Link component="button" underline="none" sx={{ paddingLeft: 0.5, marginBottom: { xs: 0.45, md: 0.35 }, fontWeight: 600 }} onClick={handleClose}>Log In</Link>
           </Box>
 
           <form noValidate autoComplete="off">
@@ -86,30 +59,20 @@ const LoginDialog = ({ open, handleClose }: Props) => {
                 </InputAdornment>
               ),
             }} sx={{ paddingBottom: 2 }} />
-            <FormLabel>Password</FormLabel>
-            <TextField fullWidth required type="password" InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <KeyIcon />
-                </InputAdornment>
-              ),
-            }} sx={{ paddingBottom: 0.5 }} />
           </form>
 
           <Box sx={{ display: 'flex', flexDirection: 'column' }} >
-            <Link component="button" underline='none' sx={{ marginBottom: 3 }} align='right' onClick={handleForgotOpen}>Forgot your password?</Link>
-            <Button variant="contained" sx={{ padding: 2, textTransform: 'none', textAlign: 'left' }} >
-              <Typography flexGrow={1} textAlign="center">Sign In</Typography>
+            <Button variant="contained" sx={{ padding: 2, textTransform: 'none', textAlign: 'left', marginTop: 3 }} >
+              <Typography flexGrow={1} textAlign="center">Reset Password</Typography>
               <ArrowRightAltIcon />
             </Button>
           </Box>
 
         </Box>
-        <SignupDialog open={signupOpen} handleClose={handleSignupClose} />
-        <ForgotPassword open={forgotOpen} handleClose={handleForgotClose} />
+
       </Box>
     </Dialog >
   )
 }
 
-export default LoginDialog;
+export default ForgotPassword;
